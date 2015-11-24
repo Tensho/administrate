2480b9f4d3fc83cf8678cfe74c18763eada84b04
Andrew Babichev <andrew.babichev@gmail.com>
2015-11-24 23:13:41 +0200

    Pass resource to partial locals

--- app/helpers/administrate/application_helper.rb
+++ app/helpers/administrate/application_helper.rb
@@ -1,7 +1,7 @@
 module Administrate
   module ApplicationHelper
-    def render_field(field, locals = {})
-      locals.merge!(field: field)
+    def render_field(field, resource, locals = {})
+      locals.merge!(resource: resource, field: field)
       render locals: locals, partial: field.to_partial_path
     end
 
